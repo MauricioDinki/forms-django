4fd23c586fe53a114496ae0f374e68d524f21a80
Mauricio Ivan Mejia Ramirez <101mejiaramirez@gmail.com>
2014-11-11 13:17:19 -0600

    Vista Para Procesar El Formulario

--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from .forms import DatosPersonalesForm
 
-# Create your views here.
+def FormularioView(request):
+	template = 'formulario.html'
+	if request.method == 'POST': # Si el metodo es POST
+		form = DatosPersonalesForm(request.POST) # Iniciamos EL Formulario
+		if form.is_valid(): #Si Pasa todas las validaciones que definimos en el forms.py
+			return HttpResponseRedirect('/sucess')
+	else: #Si el metodo es get solo mostramos el formulario
+		form = DatosPersonalesForm()
+
+	return render_to_response(template, {'form':form},context_instance=RequestContext(request))
