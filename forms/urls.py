2cc2d314ae92f3845e5bb656291a70e8e3531150
Mauricio Ivan Mejia Ramirez <101mejiaramirez@gmail.com>
2014-11-11 13:39:27 -0600

    Archivos De Configuracion

--- forms/urls.py
+++ forms/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'forms.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'app.views.FormularioView', name='Formulario'),
 )
