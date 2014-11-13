9efe62e153ab996873c15dc91c7a98dcbb91dc19
Mauricio Ivan Mejia Ramirez <101mejiaramirez@gmail.com>
2014-11-13 17:38:14 -0600

    actualizada la vista del FunctionView

--- app/views.py
+++ app/views.py
@@ -1,21 +1,19 @@
-from .forms import DatosPersonalesForm,LoginForm
+from .forms import InformacionForm,LoginForm
 from django.contrib.auth import login
-from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.generic import FormView
 
 # Formulario Usando FunctionView
 def FormularioView(request):
-	template = 'formulario.html'
 	if request.method == 'POST': # Si el metodo es POST
-		form = DatosPersonalesForm(request.POST) # Iniciamos EL Formulario
+		form = InformacionForm(request.POST) # Iniciamos EL Formulario
 		if form.is_valid(): #Si Pasa todas las validaciones que definimos en el forms.py
-			form = DatosPersonalesForm()
-			return render_to_response(template, {'form':form,'success':'EL Formulario es Correcto'},context_instance=RequestContext(request))
+			form = InformacionForm()
+			return render_to_response('informacion-form.html', {'form':form,'success':'El Formulario es Correcto'},context_instance=RequestContext(request))
 	else: #Si el metodo es get solo mostramos el formulario
-		form = DatosPersonalesForm()
-	return render_to_response(template, {'form':form},context_instance=RequestContext(request))
+		form = InformacionForm()
+	return render_to_response('informacion-form.html', {'form':form},context_instance=RequestContext(request))
 
 
 # Ejemplo de Login Usando FormView
