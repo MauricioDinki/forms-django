e7cf6267ba3f809ed4a0adc5a72b9e02c0753159
Mauricio Ivan Mejia Ramirez <101mejiaramirez@gmail.com>
2014-11-13 21:47:10 -0600

    añadida url para info/

--- forms/urls.py
+++ forms/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'app.views.FormularioView', name='Formulario'),
+    url(r'^info/$', 'app.views.FormularioView', name='Formulario'),
     url(r'^login/$', LoginView.as_view(), name='login'),
 )
