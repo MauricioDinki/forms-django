e3c3e2ac19c29a02834c8d2f8dd177ed77d1aebe
Mauricio Ivan Mejia Ramirez <101mejiaramirez@gmail.com>
2014-11-14 00:39:39 -0600

    añadido modelo persona

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Persona(models.Model):
+	user = models.OneToOneField(User)
+	edad = models.IntegerField(null=False, blank=False)
+
+	def __unicode__(self):
+		return self.user.username
