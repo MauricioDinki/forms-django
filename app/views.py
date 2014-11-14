786a3352de3ed9f6d2ba1436e80eadc6dd719485
Mauricio Ivan Mejia Ramirez <101mejiaramirez@gmail.com>
2014-11-13 21:46:23 -0600

    comentado el codigo de las vista

--- app/views.py
+++ app/views.py
@@ -15,7 +15,6 @@ def FormularioView(request):
 		form = InformacionForm()
 	return render_to_response('informacion-form.html', {'form':form},context_instance=RequestContext(request))
 
-
 # Ejemplo de Login Usando FormView
 class LoginView(FormView):
 	# Asignamos la plantilla html
