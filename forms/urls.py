6926e56ba3cfe0b560b160a2461cfc4f6687673a
Mauricio Ivan Mejia Ramirez <101mejiaramirez@gmail.com>
2014-11-14 00:41:18 -0600

    añadida url de registro

--- forms/urls.py
+++ forms/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from app.views import LoginView
+from app.views import LoginView,RegisterView
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^info/$', 'app.views.FormularioView', name='Formulario'),
+    url(r'^info/$', 'app.views.FormularioView', name='datos_personales'),
     url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^register/$', RegisterView.as_view(), name='register'),
 )
