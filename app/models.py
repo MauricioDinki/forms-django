18cfa6e34e08f63574da668d567c87b12cdf8e4f
Mauricio Ivan Mejia Ramirez <101mejiaramirez@gmail.com>
2014-11-17 16:55:36 -0600

    añadido campo genero

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Persona(models.Model):
 	user = models.OneToOneField(User)
 	edad = models.IntegerField(null=False, blank=False)
+	genero = models.CharField(null=False, blank=False, max_length=50,)
 
 	def __unicode__(self):
 		return self.user.username
