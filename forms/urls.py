c69fb21128b3caf84ec3af8356e9fcd053d4b131
Mauricio Ivan Mejia Ramirez <101mejiaramirez@gmail.com>
2014-11-17 17:11:33 -0600

    añadidas urls de plantillas estatica

--- forms/urls.py
+++ forms/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from app.views import LoginView,RegisterView
+from app.views import LoginView,RegisterView,IndexView,SuccessView
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', IndexView.as_view(), name='inicio'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^info/$', 'app.views.FormularioView', name='datos_personales'),
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^register/$', RegisterView.as_view(), name='register'),
+    url(r'^success/$', SuccessView.as_view(), name='success'),
 )
