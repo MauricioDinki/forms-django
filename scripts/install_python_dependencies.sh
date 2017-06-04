6ead5a29de49eb08ade1f883554751b21e626678
Mauricio Ivan Mejía Ramirez <mauriciodinki@gmail.com>
2017-06-04 14:12:57 -0500

    Update chown methods

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+chown ubuntu:ubuntu /home/ubuntu/forms
 virtualenv /home/ubuntu/venv
+chown ubuntu:ubuntu /home/ubuntu/venv
+chown ubuntu:ubuntu /home/ubuntu/venv/*
 source /home/ubuntu/venv/bin/activate
 pip install -r /home/ubuntu/forms/requirements.txt
