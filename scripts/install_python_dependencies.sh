bfc6b0b57f807b9a79e6d74aac4ae694664b66ef
Mauricio Ivan Mejía Ramirez <mauriciodinki@gmail.com>
2017-06-04 12:58:08 -0500

    Fixed script

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 virtualenv /home/ubuntu/venv
 source /home/ubuntu/venv/bin/activate
-pip install -r /home/ubuntu/forms/requirements.tx
+pip install -r /home/ubuntu/forms/requirements.txt
