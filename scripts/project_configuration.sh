10d769004affca55d7274f0f212683f007c42c6d
Mauricio Ivan Mejía Ramirez <mauriciodinki@gmail.com>
2017-06-05 12:13:39 -0500

    Update file

--- scripts/project_configuration.sh
+++ scripts/project_configuration.sh
@@ -6,7 +6,7 @@ python manage.py makemigrations
 python manage.py migrate
 cd /home/ubuntu/
 cp -rf /home/ubuntu/forms/deploy /home/ubuntu/
-chmode +x /home/ubuntu/deploy/deploy
+chmod +x /home/ubuntu/deploy/deploy
 sudo cp /home/ubuntu/forms/deploy/forms /etc/nginx/sites-enabled
 sudo systemctl restart nginx
 sudo cp /home/ubuntu/forms/deploy/forms.conf /etc/supervisor/conf.d/
