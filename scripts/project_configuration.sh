fa5b3e5e7d7bf351cef001501544c5712022e5ec
Mauricio Ivan Mejía Ramirez <mauriciodinki@gmail.com>
2017-06-05 11:55:54 -0500

    Update script

--- scripts/project_configuration.sh
+++ scripts/project_configuration.sh
@@ -8,7 +8,7 @@ cd /home/ubuntu/
 cp -rf /home/ubuntu/forms/deploy /home/ubuntu/
 sudo cp /home/ubuntu/forms/deploy/forms /etc/nginx/sites-enabled
 sudo systemctl restart nginx
-sudo cp /home/ubuntu/forms/deploy/app.conf /etc/supervisor/conf.d/
+sudo cp /home/ubuntu/forms/deploy/forms.conf /etc/supervisor/conf.d/
 sudo systemctl restart supervisor
 sudo systemctl enable supervisor
 sudo supervisorctl start forms
