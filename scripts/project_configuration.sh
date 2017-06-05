5175610bbaa9999153f630f9aec5cc21e9dfeb49
Mauricio Ivan Mejía Ramirez <mauriciodinki@gmail.com>
2017-06-05 11:42:00 -0500

    Add supervisor configuration

--- scripts/project_configuration.sh
+++ scripts/project_configuration.sh
@@ -8,3 +8,6 @@ cd /home/ubuntu/
 cp -rf /home/ubuntu/forms/deploy /home/ubuntu/
 sudo cp /home/ubuntu/forms/deploy/forms /etc/nginx/sites-enabled
 sudo systemctl restart nginx
+sudo cp /home/ubuntu/forms/deploy/app.conf /etc/supervisor/conf.d/
+sudo systemctl restart supervisor
+sudo systemctl enable supervisor
