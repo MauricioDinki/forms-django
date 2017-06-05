aa51879706b40fc187dcda4d9cb8f49f22d6f4fe
Mauricio Ivan Mejía Ramirez <mauriciodinki@gmail.com>
2017-06-05 14:58:26 -0500

    Update scripts for app

--- scripts/project_configuration.sh
+++ scripts/project_configuration.sh
@@ -4,12 +4,3 @@ source venv/bin/activate
 cd forms
 python manage.py makemigrations
 python manage.py migrate
-cd /home/ubuntu/
-cp -rf /home/ubuntu/forms/deploy /home/ubuntu/
-chmod +x /home/ubuntu/deploy/deploy
-sudo cp /home/ubuntu/forms/deploy/forms /etc/nginx/sites-enabled
-sudo systemctl restart nginx
-sudo cp /home/ubuntu/forms/deploy/forms.conf /etc/supervisor/conf.d/
-sudo systemctl restart supervisor
-sudo systemctl enable supervisor
-sudo supervisorctl start forms
--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,1 +1,12 @@
-echo "Start application"
+#!/usr/bin/env bash
+cd /home/ubuntu/
+cp -rf /home/ubuntu/forms/deploy /home/ubuntu/
+chmod +x /home/ubuntu/deploy/deploy
+
+sudo cp /home/ubuntu/forms/deploy/forms /etc/nginx/sites-enabled
+sudo systemctl restart nginx
+
+sudo cp /home/ubuntu/forms/deploy/forms.conf /etc/supervisor/conf.d/
+sudo systemctl restart supervisor
+sudo systemctl enable supervisor
+sudo supervisorctl start forms
