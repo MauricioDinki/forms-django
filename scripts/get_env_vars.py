4e9743a2cb4a12b4f529f5b6faa056858d70f5c1
Mauricio Ivan Mejía Ramirez <mauriciodinki@gmail.com>
2017-06-05 19:52:19 -0500

    Add get env vars function

---
+++ scripts/get_env_vars.py
@@ -0,0 +1,18 @@
+import boto
+
+import sys, os
+
+from boto.s3.key import Key
+
+LOCAL_PATH = './'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+bucket_name = 'django-forms'
+# connect to the bucket
+conn = boto.connect_s3(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+bucket = conn.get_bucket(bucket_name)
+# go through the list of files
+bucket_list = bucket.list()
+for l in bucket_list:
+  keyString = "enviroment"
+  l.get_contents_to_filename(LOCAL_PATH+keyString)
