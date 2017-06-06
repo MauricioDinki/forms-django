2b1abfec3649167dd8d81008e6c5abf2dcd7a056
Mauricio Ivan Mejía Ramirez <mauriciodinki@gmail.com>
2017-06-06 15:34:16 -0500

    Update aws

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -3,5 +3,6 @@ sudo chown ubuntu:ubuntu /home/ubuntu/forms
 virtualenv /home/ubuntu/venv
 sudo chown ubuntu:ubuntu /home/ubuntu/venv
 sudo chown ubuntu:ubuntu /home/ubuntu/venv/*
+sudo chown ubuntu:ubuntu /home/ubuntu/.aws/*
 source /home/ubuntu/venv/bin/activate
 pip install -r /home/ubuntu/forms/requirements.txt
