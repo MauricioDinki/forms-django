bfa05d8872eb3b34c333160805d939a5c89d5f48
Mauricio Ivan Mejía Ramirez <mauriciodinki@gmail.com>
2017-06-06 18:35:20 -0500

    Update scripts

--- scripts/clean_instance.sh
+++ scripts/clean_instance.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 rm -rf /home/ubuntu/forms
 rm -rf /home/ubuntu/venv
+rm -rf /home/ubuntu/deploy
--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -4,6 +4,7 @@ cp -rf /home/ubuntu/forms/deploy /home/ubuntu/
 chmod +x /home/ubuntu/deploy/deploy
 
 sudo cp /home/ubuntu/forms/deploy/forms /etc/nginx/sites-enabled
+sudo rm /etc/nginx/sites-enabled/default
 sudo systemctl restart nginx
 
 sudo cp /home/ubuntu/forms/deploy/forms.conf /etc/supervisor/conf.d/
