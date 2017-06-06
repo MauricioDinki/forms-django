eb79a4797366db67d343df45f2e86713d3329097
Mauricio Ivan Mejía Ramirez <mauriciodinki@gmail.com>
2017-06-06 14:26:05 -0500

    Update manage.py and wsgi

--- forms/wsgi.py
+++ forms/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "forms.settings")
-
+import dotenv
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -2,9 +2,9 @@
 import os
 import sys
 
+import dotenv
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "forms.settings")
-
+    dotenv.read_dotenv()
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
